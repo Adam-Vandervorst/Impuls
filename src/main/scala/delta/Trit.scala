87c2bb06b35b78328025ad0a3a32776aeda7752c
Adam Vandervorst <adam.vandervorst@gmail.com>
2023-02-16 16:54:25 +0100

    Trit WIP

---
+++ src/main/scala/delta/Trit.scala
@@ -0,0 +1,110 @@
+package be.adamv.impuls
+package delta
+
+import be.adamv.momentum.{*, given}
+import be.adamv.impuls.*
+import be.adamv.impuls.delta.BitDelta.Flip
+
+
+type Trit = -1 | 0 | 1
+
+
+enum TritDelta extends Delta[Trit]:
+  case ToPos, ToNeg, Neutralize, Negate
+import TritDelta.*
+
+
+trait TritHandle extends UDeltaSink[Trit, TritDelta]:
+  self =>
+  lazy val toPosHandle: Sink[Unit, Unit] = self.dsink.contramapTo(ToPos)
+  lazy val toNegHandle: Sink[Unit, Unit] = self.dsink.contramapTo(ToNeg)
+  lazy val neutralizeHandle: Sink[Unit, Unit] = self.dsink.contramapTo(Neutralize)
+  lazy val negateHandle: Sink[Unit, Unit] = self.dsink.contramapTo(Negate)
+
+  lazy val oppositeHandle: TritHandle = new TritHandle:
+    override def set(v: Trit): Unit = self.set((-v).asInstanceOf[Trit])
+
+    override val dsink: Sink[TritDelta, Unit] = self.dsink.contramap {
+      case ToPos => ToNeg
+      case ToNeg => ToPos
+      case x => x
+    }
+
+
+trait TritView extends UDeltaSource[Trit, TritDelta]:
+  self =>
+  lazy val toPosView: Descend[Unit, Unit, Unit] = self.dsource.collect{ case ToPos => () }
+  lazy val toNegView: Descend[Unit, Unit, Unit] = self.dsource.collect{ case ToNeg => () }
+  lazy val neutralizeView: Descend[Unit, Unit, Unit] = self.dsource.collect{ case Neutralize => () }
+  lazy val negateView: Descend[Unit, Unit, Unit] = self.dsource.collect{ case Negate => () }
+
+  lazy val oppositeView: TritView = new TritView:
+    override def get(u: Unit): Trit = (-self.value).asInstanceOf[Trit]
+
+    override val dsource: Descend[Unit, TritDelta, Unit] = self.dsource.map{
+      case ToPos => ToNeg
+      case ToNeg => ToPos
+      case x => x
+    }
+
+  lazy val abs: BitView = new:
+    override def get(u: Unit): Boolean = self.value != 0
+
+    override val dsource: Descend[Unit, BitDelta, Unit] = self.dsource.collect {
+      case ToPos => BitDelta.Rise
+      case ToNeg =>  BitDelta.Rise
+      case Neutralize =>  BitDelta.Fall
+    }
+
+  lazy val higher: BitView = new:
+    override def get(u: Unit): Boolean = self.value != -1
+
+    override val dsource: Descend[Unit, BitDelta, Unit] = self.dsource.collect {
+      case ToPos => BitDelta.Rise
+      case ToNeg => BitDelta.Fall
+      case Neutralize => BitDelta.Fall
+      case Flip => ???
+    }
+
+
+//def higher: Source[Boolean] = ???
+//// ToPos.mapTo(true) | Negate.map(_ == 1) | Neutralize.mapTo(false)
+//def lower: Source[Boolean] = ???
+//// ToNeg.mapTo(true) | Negate.map(_ == 0) | Neutralize.mapTo(false)
+//def riseFrom: Source[Boolean] = ???
+//// ToPos.mapTo(true) | ToNeg.mapTo(false)
+//def fallFrom: Source[Boolean] = ???
+//// Neutralize.map(???)
+
+
+class TritRelayVar(initial: Trit) extends UDeltaRelayVar[Trit, TritDelta](initial), TritHandle, TritView:
+  self =>
+  def integration(dt: TritDelta): Trit = dt match
+    case ToPos => 1
+    case ToNeg => -1
+    case Neutralize => 0
+    case Negate => (-value).asInstanceOf[Trit]
+
+  def setPos(): Boolean =
+    if value == 1 then false
+    else
+      integrate(if value == 0 then ToPos else Negate)
+      true
+
+  def setNeg(): Boolean =
+    if value == -1 then false
+    else
+      integrate(if value == 0 then ToNeg else Negate)
+      true
+
+  def setZero(): Boolean =
+    if value == 0 then false
+    else
+      integrate(Neutralize)
+      true
+
+  def negate(): Boolean =
+    if value == 0 then false
+    else
+      integrate(Negate)
+      true
